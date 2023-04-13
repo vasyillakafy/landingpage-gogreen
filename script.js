4eebc41e54c38ada83b374ff40ea44e977a15817
vasyillakafy <vasyillakafy@gmail.com>
2023-04-13 18:54:46 +0700

    mengubah logic js sesuai soal

--- script.js
+++ script.js
@@ -1,40 +1,48 @@
 // Form
+function isNumber(num) {
+  return !isNaN(num);
+}
+
 function checkboxIsChecked() {
-    return document.getElementById("status").checked;
+  return document.getElementById("status").checked;
+}
+
+function handleGetFormData() {
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const city = document.getElementById("city").value;
+  const zipCode = document.getElementById("zip-code").value;
+  const status = document.getElementById("status").checked;
+  const props = {
+    name: name,
+    email: email,
+    city: city,
+    zipCode: zipCode,
+    status: status,
+  };
+  return props;
+}
+
+function validateFormData(props) {
+  if (props != null && isNumber(props.zipCode) && checkboxIsChecked()) {
+    return true;
+  } else {
+    return false;
   }
-  
-  function handleGetFormData() {
-    const email = document.getElementById("email").value;
-    const msg = document.getElementById("msg").value;
-    const status = document.getElementById("status").checked;
-    const props = {
-      msg: msg,
-      email: email,
-      status: status,
-    };
-    return props;
+}
+
+document.getElementById("submit-form").addEventListener("click", function (event) {
+  event.preventDefault();
+  submit();
+});
+
+function submit() {
+  event.preventDefault;
+  const props = handleGetFormData();
+  const hasil = validateFormData(props);
+  if (hasil == false) {
+    document.getElementById("warning").innerText = "Periksa form anda sekali lagi!";
+  } else {
+    return hasil;
   }
-  
-  function validateFormData(props) {
-    if (props != null && checkboxIsChecked()) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-  
-  document.getElementById("btn-send").addEventListener("click", function (event) {
-    event.preventDefault();
-    submit();
-  });
-  
-  function submit() {
-    event.preventDefault;
-    const props = handleGetFormData();
-    const hasil = validateFormData(props);
-    if (hasil == false) {
-      document.getElementById("warning").innerText = "Periksa form anda sekali lagi!";
-    } else {
-      return hasil;
-    }
-  }
+}
